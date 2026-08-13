489036443e3790f617761901a090d183afaf3b67
mac <@b.tunde.ipaye@gmail.com>
2026-08-13 17:39:40 +0100

    feat: clerk provider updated commit by ENGR. IPAYE

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,14 +14,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
     return (
         <ClerkProvider>
-        <html lang="en">
-            <body className={`${outfit.className} antialiased`}>
-                <StoreProvider>
-                    <Toaster />
-                    {children}
-                </StoreProvider>
-            </body>
-        </html>
+            <html lang="en">
+                <body className={`${outfit.className} antialiased`}>
+                    <StoreProvider>
+                        <Toaster />
+                        {children}
+                    </StoreProvider>
+                </body>
+            </html>
         </ClerkProvider>
     );
 }
