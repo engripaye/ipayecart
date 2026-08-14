c3f53e92a3a2420f4aca3ed2903f2d57e47bfffb
mac <@b.tunde.ipaye@gmail.com>
2026-08-14 17:01:31 +0100

    feat: inngest functions updated commit by ENGR. IPAYE

---
+++ inngest/functions.js
@@ -0,0 +1,19 @@
+import { inngest } from './client';
+import prisma from '@/lib/prisma';
+// ingest function to save user to a database
+
+export const syncUserCreation = inngest.createFunction(
+    {id: 'sync-user-create'},
+    {event: 'clerk/user.created'},
+    async ({ event }) => {
+        const {data} = event
+        await prisma.user.create({
+            data: {
+                id: data.id,
+                email: data.email_address[0].email_address,
+                name: `${data.first_name} ${data.last_name}`,
+                image: data.image_url,
+            }
+        })
+    }
+)
