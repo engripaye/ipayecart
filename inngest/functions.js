bfd30cce167f04debb9ff01d58ec40b7b6320401
mac <@b.tunde.ipaye@gmail.com>
2026-08-14 17:04:56 +0100

    feat: inngest functions data.id updated commit by ENGR. IPAYE

--- inngest/functions.js
+++ inngest/functions.js
@@ -18,3 +18,26 @@ export const syncUserCreation = inngest.createFunction(
         })
     }
 )
+
+export const syncUserUpdate = inngest.createFunction(
+    {id: 'sync-user-update'},
+    {event: 'clerk/user.updated'},
+    async ({ event }) => {
+        const {data} = event
+        await prisma.user.update({
+            where: {
+                id: data.id,
+            },
+
+            data: {
+                id: data.id,
+                email: data.email_address[0].email_address,
+                name: `${data.first_name} ${data.last_name}`,
+                image: data.image_url,
+
+            }
+
+
+        })
+    }
+)
