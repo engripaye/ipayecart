01bdc098cedf82c7f0bf9ff1ca31742af8b5895d
mac <@b.tunde.ipaye@gmail.com>
2026-08-15 18:55:27 +0100

    feat: function create sync updated commit by ENGR. IPAYE

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,41 +1,25 @@
-import { inngest } from "./client";
-import prisma from "@/lib/prisma";
+import { inngest } from './client';
+import prisma from '@/lib/prisma';
 
 // Sync newly created Clerk users to the database
 export const syncUserCreation = inngest.createFunction(
     {
-        id: "sync-user-create",
-        triggers: {
-            event: "clerk/user.created",
-        },
-    },
-    async ({ event }) => {
-        const data = event.data;
-
-        if (!data?.id) {
-            throw new Error("Clerk user data is missing");
-        }
-
-        const email = data.email_addresses?.[0]?.email_address;
-
-        if (!email) {
-            throw new Error(
-                `No email address found for Clerk user ${data.id}`
-            );
-        }
-
-        const name = `${data.first_name ?? ""} ${data.last_name ?? ""}`.trim();
-
+        id: "sync-user-create"},
+        {event: 'clerk/user.created'},
+    async({params}) => {
+        const {data} = event
         await prisma.user.create({
             data: {
                 id: data.id,
-                email,
-                name,
-                image: data.image_url ?? null,
-            },
-        });
+                email: data.email_address[0].email_address,
+                name: `${data.first_name} ${data.last_name}`,
+                image: data.image_url,
+            }
+        })
+
     }
-);
+)
+
 
 // Sync updated Clerk users to the database
 export const syncUserUpdate = inngest.createFunction(
