41e8499da2fc0b076244c18c1d40b27247350efc
mac <@b.tunde.ipaye@gmail.com>
2026-08-17 17:14:07 +0100

    feat: image kit updated commit by ENGR. IPAYE

--- configs/imageKit.js
+++ configs/imageKit.js
@@ -4,4 +4,5 @@ export const imageKit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
     urlEndPoint: process.env.IMAGEKIT_URL_ENDPOINT
+
 });
