2ef0b01e82c06ceb16c16166d250f369b33d44e4
mac <@b.tunde.ipaye@gmail.com>
2026-08-18 19:28:37 +0100

    feat: routes updated commit by ENGR. IPAYE

--- app/api/store/create/route.js
+++ app/api/store/create/route.js
@@ -2,7 +2,7 @@ import { getAuth } from "@clerk/nextjs/server";
 import {NextResponse} from "next/server";
 import prisma from "@/lib/prisma";
 
-// create a storee
+// create a store
 export async function POST(request) {
 
     try {
