3614e251562b2be300fd95cc86bdd5bf385d04b2
mac <@b.tunde.ipaye@gmail.com>
2026-08-25 18:54:48 +0100

    feat: dashboard route, get all orders updated commit by ENGR. IPAYE

--- app/api/store/dashboard/route.js
+++ app/api/store/dashboard/route.js
@@ -1,5 +1,6 @@
 import {NextResponse} from "next/server";
 import {getAuth} from "@clerk/nextjs/server";
+import authSeller from "@/middleware/authSeller";
 
 // get dashboard data for the seller
 
@@ -7,7 +8,13 @@ import {getAuth} from "@clerk/nextjs/server";
 
 export async function GET(request){
     try{
-        const { userId } = getAuth()
+        const { userId } = getAuth(request)
+        const storeId = await authSeller(userId)
+
+        // get all orders
+        const orders = await prisma.order.findMany({where: {storeId}})
+
+        // get all product with rating
     }catch (error){
         console.error(error);
         return NextResponse.json({ error: error.code || error.message }, { status: 400})
