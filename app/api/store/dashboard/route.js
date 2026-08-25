4d3c63c12a41dea83bc8d2249510df20398d271a
mac <@b.tunde.ipaye@gmail.com>
2026-08-25 18:53:03 +0100

    feat: dashboard route updated commit by ENGR. IPAYE

--- app/api/store/dashboard/route.js
+++ app/api/store/dashboard/route.js
@@ -1,3 +1,15 @@
-
+import {NextResponse} from "next/server";
+import {getAuth} from "@clerk/nextjs/server";
 
 // get dashboard data for the seller
+
+
+
+export async function GET(request){
+    try{
+        const { userId } = getAuth()
+    }catch (error){
+        console.error(error);
+        return NextResponse.json({ error: error.code || error.message }, { status: 400})
+    }
+}
