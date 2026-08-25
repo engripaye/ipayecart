dffd6e39457237c0c835f21aaefa399e8616bc98
mac <@b.tunde.ipaye@gmail.com>
2026-08-25 14:44:28 +0100

    feat: uploading images impl. commit by ENGR. IPAYE

--- app/api/store/product/route.js
+++ app/api/store/product/route.js
@@ -1,5 +1,6 @@
 import {getAuth} from "@clerk/nextjs/server";
 import authSeller from "@/middleware/authSeller";
+import imageKit from "@/configs/imageKit";
 
 // Add a new product
  export async function POST(request) {
@@ -15,11 +16,28 @@ import authSeller from "@/middleware/authSeller";
         const formData = await request.formData();
         const name = formData.get("name");
         const description = formData.get("description");
-        const mrp = formData.get("name");
-        const price = formData.get("price");
+        const mrp = Number(formData.get("mrp"));
+        const price = Number(formData.get("price"));
         const category = formData.get("category");
-        const image = formData.get("image");
+        const images = formData.getAll("images");
 
+        if(!name || !description || !mrp || !price || !category || images.length < 1) {
+            return NextResponse.json({ error: "Missing product details" }, { status: 400 });
+        }
+
+        //uploading images to image kit
+        const imagesUrl = await Promise.all(images.map(async (image) => {)
+            const buffer = Buffer.from(await image.arrayBuffer());
+            const response = await imageKit.upload({
+                file: buffer,
+                fileName: image.name,
+                folder: "products",
+
+            })
+            const url = imageKit.url({[
+                path: response.filePath,
+
+                    ]})
     }catch (error) {
 
     }
