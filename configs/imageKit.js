7f91a4af247328f877e8d91fbcd1246e49e9f1e6
mac <@b.tunde.ipaye@gmail.com>
2026-08-26 15:31:52 +0100

    feat: image kit updated commit by ENGR. IPAYE

--- configs/imageKit.js
+++ configs/imageKit.js
@@ -3,7 +3,7 @@ import ImageKit from "imagekit";
 export const imageKit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
-    urlEndPoint: process.env.IMAGEKIT_URL_ENDPOINT
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 });
 
 export default imageKit;
