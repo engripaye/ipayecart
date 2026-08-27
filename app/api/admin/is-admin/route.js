e87066ea6a5db21b9fe4159e7f9b828e96705c79
mac <@b.tunde.ipaye@gmail.com>
2026-08-27 01:33:40 +0100

    feat: api admin impl updated commit by ENGR. IPAYE

---
+++ app/api/admin/is-admin/route.js
@@ -0,0 +1,23 @@
+import {getAuth} from "@clerk/nextjs/server";
+import authAdmin from "@/middleware/authAdmin";
+import {NextResponse} from "next/server";
+// auth Admin
+
+
+export async function GET(request){
+    try{
+        const { userId } = getAuth(request);
+        const isAdmin = await authAdmin(userId)
+
+        if(!isAdmin) {
+            return NextResponse.json({error: "Not Authorized"}, {status: 401})
+
+        }
+        return NextResponse.json({isAdmin})
+
+
+        }catch (error){
+
+
+    }
+}
