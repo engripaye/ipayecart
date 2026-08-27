e47f9dfb7de9d24ce0a55ec4a6cd30fd528deb0f
mac <@b.tunde.ipaye@gmail.com>
2026-08-27 14:38:03 +0100

    feat: api admin, dashboard data updated, error commit by ENGR. IPAYE

--- app/api/admin/dashboard/route.js
+++ app/api/admin/dashboard/route.js
@@ -50,6 +50,7 @@ export async function GET(request){
         return NextResponse.json({dashboardData})
     }catch (error){
         console.error(error);
+        return NextResponse.json({ error: error.code || error.message }, {status: 400})
     }
 
 }
