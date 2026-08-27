3a14521ddb3aa41056511f75b92fb26cf6d1f8bb
mac <@b.tunde.ipaye@gmail.com>
2026-08-27 14:28:04 +0100

    feat: api admin, dashboard data updated commit by ENGR. IPAYE

--- app/api/admin/dashboard/route.js
+++ app/api/admin/dashboard/route.js
@@ -4,3 +4,17 @@ import {NextResponse} from "next/server";
 
 // Get dashboard data for admin
 
+export async function GET(request){
+    const { userId } = getAuth(request)
+    const isAdmin = await  authAdmin(userId)
+
+    if(isAdmin){
+        return NextResponse.json({ error: 'not authorized'}, { status: 401 });
+    }
+
+    // get total orders
+    const orders = await prisma.order.count()
+
+
+}
+
