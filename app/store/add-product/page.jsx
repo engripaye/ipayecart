af23f625f1d10efe85798dcdaa0a3ccf860ccdbc
mac <@b.tunde.ipaye@gmail.com>
2026-08-31 17:39:53 +0100

    refractor: add store pages updated commit by ENGR. IPAYE

--- app/store/add-product/page.jsx
+++ app/store/add-product/page.jsx
@@ -3,6 +3,7 @@ import { assets } from "@/assets/assets"
 import Image from "next/image"
 import { useState } from "react"
 import { toast } from "react-hot-toast"
+import {useAuth} from "@clerk/nextjs";
 
 export default function StoreAddProduct() {
 
@@ -18,6 +19,8 @@ export default function StoreAddProduct() {
     })
     const [loading, setLoading] = useState(false)
 
+    const {getToken} = useAuth()
+
 
     const onChangeHandler = (e) => {
         setProductInfo({ ...productInfo, [e.target.name]: e.target.value })
@@ -26,6 +29,23 @@ export default function StoreAddProduct() {
     const onSubmitHandler = async (e) => {
         e.preventDefault()
         // Logic to add a product
+        try{
+            // if no images are uploaded then return
+            if(!images[1] && !images[2] && !images[3] && !images[4]{
+                return toast.error('Please upload at least one image')
+            }
+            setLoading(true)
+
+            const formData = new FormData()
+            formData.append('name', productInfo.name)
+            formData.append('description', productInfo.description)
+            formData.append('mrp', productInfo.mrp)
+            formData.append('price', productInfo.price)
+            formData.append('category', productInfo.category)
+
+        }catch (error){
+
+        }
         
     }
 
