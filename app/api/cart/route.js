f48d5f3dfd9ee03c706988d7db7f267de2963f23
mac <@b.tunde.ipaye@gmail.com>
2026-09-01 03:23:51 +0100

    feat: user cart page, import updated commit by ENGR. IPAYE

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -1,4 +1,5 @@
 import {getAuth} from "@clerk/nextjs/server";
+import {NextResponse} from "next/server";
 
 
 // update user cart
@@ -8,7 +9,14 @@ export async function POST(request){
         const { cart } = await request.json()
 
         // save the cart to the user object
-        await
+        await prisma.user.update ({
+            where: {
+                id: userId
+            }, data: { cart: cart}
+        })
+        return NextResponse.json({
+            message: "cart updated"
+        })
     }catch(error){
 
     }
