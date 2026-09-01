e9cff7f668a62263f1e3e92745a3b9d73681a4ab
mac <@b.tunde.ipaye@gmail.com>
2026-09-01 02:43:34 +0100

    feat: order route updated, import, nextResponse commit by ENGR. IPAYE

--- app/api/store/orders/route.js
+++ app/api/store/orders/route.js
@@ -13,8 +13,21 @@ export async function POST(request) {
         if (!storeId) {
             return NextResponse.json({ error: 'not authorized' }, { status: 401 });
         }
+        const {orderId, status} = await request.json()
+
+        await prisma.order.update({
+            where: {
+                id: orderId, storeId,
+                data: {
+                    status
+                }
+            },
+        })
+        return NextResponse.json({ message: 'Order status updated successfully' });
 
     }catch(error){
+        console.log(error);
+        return NextResponse.json({ error: error.code || error.message }, { status: 400 });
 
     }
 }
