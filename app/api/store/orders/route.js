6f02d9eb528216a14fc84d96408dd366f7e074d6
mac <@b.tunde.ipaye@gmail.com>
2026-09-01 02:49:18 +0100

    feat: order route updated, get all seller orders commit by ENGR. IPAYE

--- app/api/store/orders/route.js
+++ app/api/store/orders/route.js
@@ -31,3 +31,34 @@ export async function POST(request) {
 
     }
 }
+
+// Get all orders for a seller
+export async function GET(request) {
+    try{
+        const { userId } = getAuth(request)
+        const storeId = await authSeller(userId)
+
+        if(!storeId) {
+            return NextResponse.json({ error: 'not authorized' }, { status: 401 });
+        }
+
+        const orders = await prisma.order.findMany({
+            where: {
+                storeId,
+            }, include: {
+            User: true, address: true, orderItems: {
+            include: {
+                product: true
+            }
+            }
+            }, orderBy: {
+                createdAt: 'desc'
+            }
+        })
+        return NextResponse.json({orders})
+        }catch (error){
+        console.log(error);
+        return NextResponse.json({error: error.code || error.message }, { status: 400})
+
+    }
+}
