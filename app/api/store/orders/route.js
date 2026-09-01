92b9127bd3d978a0c055c40b6c38d4e6f16002f5
mac <@b.tunde.ipaye@gmail.com>
2026-09-01 02:39:41 +0100

    feat: order route updated commit by ENGR. IPAYE

--- app/api/store/orders/route.js
+++ app/api/store/orders/route.js
@@ -1,4 +1,6 @@
 import {getAuth} from "@clerk/nextjs/server";
+import {NextResponse} from "next/server";
+import authSeller from "@/middleware/authSeller";
 
 // update seller order status
 
@@ -6,6 +8,11 @@ import {getAuth} from "@clerk/nextjs/server";
 export async function POST(request) {
     try{
         const { userId } = getAuth(request)
+        const storeId = await authSeller(userId)
+
+        if (!storeId) {
+            return NextResponse.json({ error: 'not authorized' }, { status: 401 });
+        }
 
     }catch(error){
 
