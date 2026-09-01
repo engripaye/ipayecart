8882ccc1066486a4db1187d7281e501f3570d95a
mac <@b.tunde.ipaye@gmail.com>
2026-09-01 15:25:50 +0100

    refractor: public layout page updated commit by ENGR. IPAYE

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -5,13 +5,30 @@ import Footer from "@/components/Footer";
 import {useEffect} from "react";
 import {useDispatch} from "react-redux";
 import {fetchProducts} from "@/lib/features/product/productSlice";
+import {useAuth, useUser} from "@clerk/nextjs";
+import {fetchCart} from "@/lib/features/cart/cartSlice";
 
 export default function PublicLayout({ children }) {
 
     const dispatch = useDispatch()
+    const { user } = useUser()
+    const {getToken} = useAuth()
+    const {cartItems} = useSelector((state) => state.cart)
+
     useEffect(() => {
         dispatch(fetchProducts({}))
     }, [])
+
+    useEffect(() => {
+        if(user){
+            dispatch(fetchCart({getToken}))
+        }
+    }, [])
+
+    useEffect(() => {
+
+    }, [])
+
     return (
         <>
             <Banner />
