5dad0c407fa6be02ced83967a21e67a533a029ca
mac <@b.tunde.ipaye@gmail.com>
2026-09-02 19:29:17 +0100

    refractor: api route page updated commit by ENGR. IPAYE

--- app/api/coupon/route.js
+++ app/api/coupon/route.js
@@ -1,13 +1,14 @@
 import {useAuth} from "@clerk/nextjs";
 import prisma from "@/lib/prisma";
 import {NextResponse} from "next/server";
+import {getAuth} from "@clerk/nextjs/server";
 
 // verify coupon
 
 
 export async function POST(request) {
     try {
-        const {userId, has} = useAuth(request)
+        const {userId, has} = getAuth(request)
         const {code} = await request.json()
 
         const coupon = await prisma.coupon.findUnique({
