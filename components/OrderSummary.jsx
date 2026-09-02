70204ce7534a5d3535c235c1bd4fe1bb59e9fb36
mac <@b.tunde.ipaye@gmail.com>
2026-09-02 02:04:52 +0100

    refractor: order summary, checkout calculation updated commit by ENGR. IPAYE

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -102,7 +102,7 @@ const OrderSummary = ({ totalPrice, items }) => {
                 <p>Total:</p>
 
                 <p className='font-medium text-right'>
-                    <Show when={{ plan: "plus"}} fallback={`${currency}${coupon ? (totalPrice - (coupon.discount / 100 * totalPrice)).toFixed(2) : totalPrice.toLocaleString()}`}>
+                    <Show when={{ plan: "plus"}} fallback={`${currency}${coupon ? (totalPrice + 5 - (coupon.discount / 100 * totalPrice)).toFixed(2) : (totalPrice + 5).toLocaleString()}`}>
 
                     </Show>
                     </p>
