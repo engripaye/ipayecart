d579851af182bcf1495b924444db8ccba791ee63
mac <@b.tunde.ipaye@gmail.com>
2026-09-02 02:18:59 +0100

    refractor: pricing table, calculation cost updated commit by ENGR. IPAYE

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -103,7 +103,7 @@ const OrderSummary = ({ totalPrice, items }) => {
 
                 <p className='font-medium text-right'>
                     <Show when={{ plan: "plus"}} fallback={`${currency}${coupon ? (totalPrice + 5 - (coupon.discount / 100 * totalPrice)).toFixed(2) : (totalPrice + 5).toLocaleString()}`}>
-
+                        {currency}{coupon ? (totalPrice - (coupon.discount / 100 * totalPrice)).toFixed(2) : totalPrice .toLocaleString()}
                     </Show>
                     </p>
             </div>
