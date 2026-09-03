7bb62ba2edbb6daeb7ed744db9d44026de519a0e
mac <@b.tunde.ipaye@gmail.com>
2026-09-03 03:00:52 +0100

    refractor: order Summary, stripe payment, error logic updated commit by ENGR. IPAYE

--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -1,17 +1,18 @@
 import { PlusIcon, SquarePenIcon, XIcon } from 'lucide-react';
 import React, { useState } from 'react'
 import AddressModal from './AddressModal';
-import { useSelector } from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
 import {Show, useAuth, useUser} from "@clerk/nextjs";
 import axios from "axios";
+import {fetchCart} from "@/lib/features/cart/cartSlice";
 
 const OrderSummary = ({ totalPrice, items }) => {
 
     const { user } = useUser()
     const { getToken } = useAuth()
-
+    const dispatch = useDispatch()
     const currency = process.env.NEXT_PUBLIC_CURRENCY_SYMBOL || '$';
 
     const router = useRouter();
@@ -101,15 +102,12 @@ const OrderSummary = ({ totalPrice, items }) => {
             }else {
                 toast.success(data.message)
                 router.push('/orders')
-
+                dispatch(fetchCart({ getToken }))
             }
 
         } catch (error) {
             toast.error(
-                error?.response?.data?.error ||
-                error.message ||
-                "Failed to place order"
-            );
+                error?.response?.data?.error || error.message);
         }
     };
 
