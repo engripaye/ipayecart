bc1e850f8c25d87b05f0e77c509837d63029af5f
mac <@b.tunde.ipaye@gmail.com>
2026-09-03 02:09:59 +0100

    feat: api order route, coupon code logic updated commit by ENGR. IPAYE

--- app/api/orders/route.js
+++ app/api/orders/route.js
@@ -1,5 +1,6 @@
 import {getAuth} from "@clerk/nextjs/server";
 import {NextResponse} from "next/server";
+import prisma from "@/lib/prisma";
 
 
 export async function POST(request) {
@@ -18,7 +19,46 @@ export async function POST(request) {
         let coupon = null;
 
         if(couponCode){
+            coupon = await prisma.coupon.findFirst({
+                where: { code: couponCode },
+            });
+        }
 
+
+
+        if (!coupon) {
+            return NextResponse.json(
+                { error: "Coupon not found or expired" },
+                { status: 404 }
+            );
+        }
+
+        // New-user coupon
+        if (coupon.forNewUser) {
+            const userOrders = await prisma.order.findMany({
+                where: {
+                    userId,
+                },
+            });
+
+            if (userOrders.length > 0) {
+                return NextResponse.json(
+                    { error: "Coupon valid for new users only" },
+                    { status: 400 }
+                );
+            }
+        }
+
+        // Plus-member coupon
+        if (coupon.forMember) {
+            const hasPlusPlan = has({ plan: "plus" });
+
+            if (!hasPlusPlan) {
+                return NextResponse.json(
+                    { error: "Coupon valid for Plus members only" },
+                    { status: 400 }
+                );
+            }
         }
     }catch (error){
 
