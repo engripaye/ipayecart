ef223459f7d9891437d33299a3ca078ec6d2d510
mac <@b.tunde.ipaye@gmail.com>
2026-09-05 20:03:30 +0100

    feat: paystack function impl, import logic commit by ENGR. IPAYE

--- app/api/paystack/initialiaze/route.js
+++ app/api/paystack/initialiaze/route.js
@@ -1,6 +1,16 @@
+import {getAuth} from "@clerk/nextjs/server";
+import {NextResponse} from "next/server";
 
 export async function POST(request) {
     try{
+        const { userId } = getAuth(request)
+
+        if(!userId){
+            return NextResponse.json({ error: 'not authorized' },
+                { status: 401 });
+        }
+
+
 
     }catch (error){
 
