78ee3b296b3e8543d685320593c07843d4ec1dbe
mac <@b.tunde.ipaye@gmail.com>
2026-09-07 01:28:26 +0100

    feat: paystack callback function, searchParams logic commit by ENGR. IPAYE

--- app/api/paystack/callback/route.js
+++ app/api/paystack/callback/route.js
@@ -1,9 +1,15 @@
+import {NextResponse} from "next/server";
 
 
 export async function GET(request){
     try{
         const { searchParams } = new URL(request.url);
-        const refrence = searchParams.get("reference");
+        const reference = searchParams.get("reference");
+
+        if (!reference) {
+            return NextResponse.redirect(
+                new URL("/checkout?payment=failed", request.url)
+            );
     }catch (error){
 
     }
