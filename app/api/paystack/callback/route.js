935c405484bd8c6001a0be6f429923f95fe32a7e
mac <@b.tunde.ipaye@gmail.com>
2026-09-07 01:26:30 +0100

    feat: paystack callback function, searchParams logic commit by ENGR. IPAYE

--- app/api/paystack/callback/route.js
+++ app/api/paystack/callback/route.js
@@ -2,7 +2,8 @@
 
 export async function GET(request){
     try{
-
+        const { searchParams } = new URL(request.url);
+        const refrence = searchParams.get("reference");
     }catch (error){
 
     }
