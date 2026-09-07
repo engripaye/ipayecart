b26800849f7b08181351516e909508e82f9e1e14
mac <@b.tunde.ipaye@gmail.com>
2026-09-07 01:38:31 +0100

    feat: paystack transaction not successful, orderId timestamp, prisma impl. updated commit by ENGR. IPAYE

--- app/api/paystack/callback/route.js
+++ app/api/paystack/callback/route.js
@@ -43,13 +43,19 @@ export async function GET(request){
         // IPAYE-orderId-timestamp
         const part = reference.split("-");
 
-        const prderId = part[1];
+        const orderId = part[1];
 
         if(!orderId){
             return NextResponse.redirect(
                 new URL("/checkout?payment=failed", request.url)
             );
         }
+
+        const order = await prisma.order.findUnique({
+            where : {
+                id: orderId
+            }
+        })
 
 
 
